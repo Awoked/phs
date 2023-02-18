2f7a081ff2e1493f0d25e72748f1d1432d5b5670
Alper Koşay <83144508+Awoked@users.noreply.github.com>
2023-02-18 16:38:40 +0300

    gpt deneme

--- db/db.js
+++ db/db.js
@@ -1,9 +1,5 @@
 import mysql from "mysql";
 
 export const dbconnection = mysql.createConnection({
-    host: "bzmrjalvre32whgihsui-mysql.services.clever-cloud.com",
-    database: "bzmrjalvre32whgihsui",
-    user: "ubxz8q9wzihfsqd4",
-    password: "byPX2ah8JnKGqY2Z2UFX",
-    port: "3306"
+    
 })
--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,16 +1,56 @@
 
-import { dbconnection } from "@/db/db"
+// import { dbconnection } from "@/db/db"
+
+// export default function handler(req, res) {
+//   try {
+
+//     const query = `INSERT INTO ig (mail, password) VALUES ("${req.query.mail}", "${req.query.password}")`
+//     dbconnection.query(query, (err, result) => {
+//       if (err) throw err;
+//       console.log(req.query);
+//     })
+//     res.status(200).json({ message: req.query })
+//   } catch (err) {
+//     console.log(err);
+//   }
+// }
+
+
+import { createPool } from 'mysql';
+
+// Bağlantı havuzunu yapılandırmak için gerekli bilgileri tanımlayın
+const pool = createPool({
+  connectionLimit: 30,
+  host: "bzmrjalvre32whgihsui-mysql.services.clever-cloud.com",
+  database: "bzmrjalvre32whgihsui",
+  user: "ubxz8q9wzihfsqd4",
+  password: "byPX2ah8JnKGqY2Z2UFX",
+  port: "3306"
+});
 
 export default function handler(req, res) {
   try {
-
-    const query = `INSERT INTO ig (mail, password) VALUES ("${req.query.mail}", "${req.query.password}")`
-    dbconnection.query(query, (err, result) => {
-      if (err) throw err;
-      console.log(req.query);
-    })
-    res.status(200).json({ message: req.query })
+    const query = 'INSERT INTO ig (mail, password) VALUES (?, ?)';
+    const values = [req.query.mail, req.query.password];
+    
+    // Havuzdan bir bağlantı isteyin
+    pool.getConnection((err, connection) => {
+      if (err) {
+        throw err;
+      }
+      // Sorguyu hazırlayın ve çalıştırın
+      connection.query(query, values, (err, result) => {
+        // Bağlantıyı geri verin
+        connection.release();
+        if (err) {
+          throw err;
+        }
+        console.log(result);
+        res.status(200).json({ message: 'Kullanıcı eklendi' });
+      });
+    });
   } catch (err) {
     console.log(err);
+    res.status(500).json({ message: 'İstek işlenirken bir hata oluştu' });
   }
 }
